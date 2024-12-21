2b3242095ca82fa6f56c4af2d4c41aad6af873de
Delon Newman <contact@delonnewman.name>
2024-12-21 15:39:36 -0500

    Use one assertion per example

--- test/display_test.mjs
+++ test/display_test.mjs
@@ -13,13 +13,22 @@ export class DisplayTest extends Morphic.TestCase {
 
   testNull() {
     this.assertEquals("-", display(null), 'null is displayed as "-"');
+  }
+
+  testUndefined() {
     this.assertEquals("-", display(undefined), 'undefined is displayed as "-"');
+  }
+
+  testNullObject() {
     this.assertEquals("-", display(Null), 'Null is displayed as "-"');
   }
 
-  testBoolean() {
+  testTrue() {
     this.assertEquals("Yes", display(true), 'true is displayed as "Yes"');
-    this.assertEquals("No", display(false), 'false is displayed as "No"');
+  }
+
+  testFalse() {
+    this.assertEquals("Yes", display(true), 'true is displayed as "Yes"');
   }
 
   testError() {
