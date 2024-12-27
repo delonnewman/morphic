0ba0fa9d3966ca531e8a06093741c4472d4bb6d2
Delon Newman <contact@delonnewman.name>
2024-12-27 14:46:26 -0500

    Add tests for script object

---
+++ test/script_test.mjs
@@ -0,0 +1,35 @@
+import * as Morphic from '../lib/morphic.mjs';
+const { Dispatch, JavaScript, ParameterizedMessage, VariablyParameterizedMessage, Script } = Morphic;
+
+export class ScriptTest extends Morphic.TestCase {
+  testResults() {
+    const script = Script.build();
+    script.send(Dispatch.new(JavaScript, ParameterizedMessage.new('sum', 3, 4)));
+    const result = script.run();
+
+    this.assertEquals(7, result, 'results are returned from dispatch');
+  }
+
+  testSideEffects() {
+    let called = false;
+    const effect = () => { called = true; return 11; };
+    const script = Script.build();
+    script.send(Dispatch.new(effect, ParameterizedMessage.new('call')));
+    const result = script.run();
+
+    this.assert(called, 'side effects are perfomed');
+    this.assertEquals(11, result, 'after side effects are performed results are returned');
+  }
+
+  testExtentions() {
+    Dispatch.register_extension(VariablyParameterizedMessage.new('invoke'), (subject, message) => {
+      return subject.apply(subject, message.to_arguments());
+    });
+
+    const script = Script.build();
+    script.send(Dispatch.new((x) => x + 1, VariablyParameterizedMessage.new('invoke', 3)));
+    const result = script.run();
+
+    this.assertEquals(4, result, 'extentions dispatch messages to compatible objects');
+  }
+}
--- test/tests.mjs
+++ test/tests.mjs
@@ -1,2 +1,3 @@
 export * from './assertions_test.mjs';
 export * from './display_test.mjs';
+export * from './script_test.mjs';
